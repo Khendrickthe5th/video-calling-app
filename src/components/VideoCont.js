8d9b3e1a9ae247dccebe13b200cebbf676e1562a
Ozoani Chidubem <flyinglego153971@gmail.com>
2023-10-04 16:43:45 +0100

    refactored the signal server in Nodejs from SSE Model to HTPP request model

--- src/components/VideoCont.js
+++ src/components/VideoCont.js
@@ -9,8 +9,9 @@ function VideoCont() {
   const config = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
 
   let userName;
+  let UserIntendedToConnectTo;
 
-  async function makeCall() {
+  async function makeCall(){
     const peerConnection = new RTCPeerConnection(config);
     peerConnection.onicecandidate = () => {
       console.log(peerConnection);
@@ -20,16 +21,19 @@ function VideoCont() {
     return offer;
   }
 
-  async function initiateCall(event) {
+  async function initiateCall(event){
     const peerConnection = new RTCPeerConnection(config);
-    let offer = event.target.getAttribute("data-candidate");
-    peerConnection.setRemoteDescription(
-      new RTCSessionDescription(JSON.parse(offer))
-    );
+    UserIntendedToConnectTo = event.target.innerText
+    let x = event.target.getAttribute("data-candidate")
+    let offer = JSON.parse(x).candidate
+    let offerUsername = JSON.parse(x).username
+    console.log(offerUsername, "-------", offer, "checking....")
+
+    peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
     const answer = await peerConnection.createAnswer();
     await peerConnection.setLocalDescription(answer);
     console.log(answer, "tarrr")
-     postOffersToServer(answer, userName)
+     postOffersToServer(answer, offerUsername)
     // console.log(answer);
 
     peerConnection.addEventListener("connectionstatechange", () => {
@@ -37,19 +41,21 @@ function VideoCont() {
         console.log("Peers connected!");
       }
     });
+
+    // peerConnection.addEventListener("onicecandidate", ()=>{
+    //   if
+    // })
   }
 
   const postData = async function () {
     try {
       userName = prompt("Username Please...?");
-      const res = await axios.post("http://localhost:3100/sendCredentials", {
-        mode: "cors",
-        headers: { "Content-Type": "application/json" },
-        data: { candidate: await makeCall(), username: userName },
-      });
-      setOnlineUsers(res.data);
-      console.log(res.data);
+      const res = await axios.post("http://localhost:3100/sendCredentials",
+      { candidate: await makeCall(), username: userName },
+      {mode: "cors", headers: { "Content-Type": "application/json" }});
+      setOnlineUsers(res.data.offerClients);
       setPostedData(false);
+      getUpdateFromServerAtIntervals();
     } catch (err) {
       console.log(err);
     }
@@ -60,32 +66,39 @@ function VideoCont() {
       mode: "cors",
       headers: { "Content-Type": "application/json" },
     });
-
-    if (res.data) {
-      setOnlineUsers(res.data);
-      console.log(res.data);
-      console.log("About making another request to server....");
+    if (res.data !== onlineUsers){
+      setOnlineUsers(res.data.offerClients);
+      getUpdateFromServerAtIntervals();
+      console.log(res.data, "got new update brooo")
+    }
+    else{
       getUpdateFromServerAtIntervals();
     }
+    
   };
 
   const postOffersToServer = async function(answer, currUserName){
-    const res = await axios.post("http://localhost:3100/postOffers", 
-    {
-        answer: answer,
-        username: currUserName
-    },
-    {
-    mode: "cors",
-    headers: {"Content-Type": "application/json"}
+    let res = await axios.post("http://localhost:3100/postOffers", 
+    {answer: answer, username: currUserName},
+    { mode: "cors", headers: {"Content-Type": "application/json"}
     })
-    console.log(answer, currUserName, "wizzy")
+    // .then((res)=>{console.log(res.data, "rEssy")})
+
+    res.data.answerClients.forEach((item)=>{
+      if(item.username === UserIntendedToConnectTo){
+        console.log("Yipee a username matched")
+      }
+      else{
+        console.log(item, "Didnt match any")
+      }
+    })
+    // console.log(answer, currUserName, "wizzy")
   }
 
 
   useEffect(() => {
     postedData && postData();
-    !postedData && getUpdateFromServerAtIntervals();
+    // !postedData && getUpdateFromServerAtIntervals();
   });
 
   return (
@@ -97,14 +110,8 @@ function VideoCont() {
         </div>
 
         <div>
-          {onlineUsers &&
-            onlineUsers.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  data-candidate={JSON.stringify(item.candidate)}
-                  onClick={initiateCall}
-                >
+          {onlineUsers && onlineUsers.map((item, index) => {
+              return (<div key={index} data-candidate={JSON.stringify(item)} onClick={initiateCall}>
                   {item.username}
                 </div>
               );
